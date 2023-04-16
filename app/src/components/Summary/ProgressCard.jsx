2d5f45df769ed8a716d800f4509e873f68ff6d10
JakubLatawiec <latawiec.jakub@outlook.com>
2023-04-16 09:33:17 +0200

    idk

--- app/src/components/Summary/ProgressCard.jsx
+++ app/src/components/Summary/ProgressCard.jsx
@@ -1,6 +1,20 @@
 import React from "react";
 
+import { useTasks } from "../../store/tasks-context";
+
 const ProgressCard = () => {
+  const {
+    tasksData,
+    activeTaskType,
+    tasksIteration,
+    onTaskIteration,
+    completedTasks,
+    currentTaskIndex,
+    onTaskIndex,
+    onFetchTasks,
+    onActiveTaskType,
+  } = useTasks();
+
   let today = new Date();
   let day = String(today.getDate()).padStart(2, "0");
   let month = String(today.getMonth() + 1).padStart(2, "0");
@@ -14,10 +28,10 @@ const ProgressCard = () => {
       </h3>
       <div className="flex items-center mt-[20px] justify-between">
         <p className="text-background text-[18px]">Progress</p>
-        <p className="text-background text-[18px]">100%</p>
+        <p className="text-background text-[18px]">{(completedTasks / 10) * 100}%</p>
       </div>
       <div className="mt-[10px] h-[10px] bg-cards_background rounded-md">
-        <div className="h-[100%] w-[20px] bg-blue_700 cards_background rounded-md"></div>
+        <div className={`h-[100%] w-[${(completedTasks / 10) * 100}px] bg-blue_700 cards_background rounded-md`}></div>
       </div>
       <p className="text-background text-end font-semibold text-[18px] mt-[20px] ">
         {todaysDate}
